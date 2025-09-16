414f96f5a2e16ecd40191f3d9c8f95317a9df93d
Ella B <ella55ball@gmail.com>
2025-09-16 14:12:21 +0100

    login/ sign up forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme/theme";
-
+import LogIn from "./pages/auth/LoginPage";
 import { useEffect, useRef } from "react";
 import Phaser from "phaser";
 import { GameScene } from "./phaser/scenes/GameScene";
@@ -30,16 +30,16 @@ export default function App() {
 
     return (
         <ThemeProvider theme={theme}>
-        <CssBaseline />
+            <CssBaseline />
             <div className="min-h-dvh bg-slate-900 text-white flex items-start justify-center pt-6">
+                <LogIn></LogIn>
                 <div ref={host} id="game-root" />
                 <div className="ml-4 mt-2 text-emerald-400">
                     Tailwind is working
+                </div>
             </div>
-        </div>
         </ThemeProvider>
     );
 }
 
 // test page - to be deleted when we start building the game
-
