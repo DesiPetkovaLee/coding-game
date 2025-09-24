db69fa048d42a8c8c46895a2aa6d29e32b3c2e0c
Matt <designs.sleephawk@gmail.com>
2025-09-24 19:59:21 +0100

    changed colors to COLOURS from theme created by Desi for the overlay.

--- src/phaser/prefabs/interactables/FloppyDisk.ts
+++ src/phaser/prefabs/interactables/FloppyDisk.ts
@@ -4,14 +4,14 @@ import { BaseInteractable } from "./BaseInteractable";
 // import type { Player } from "../characters/Player";
 
 export class FloppyDisk extends BaseInteractable {
-    constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-        super(scene, x, y, texture);
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+    super(scene, x, y, texture);
 
-        this.on("pointerdown", () => {
-            console.log("clicked floppydisk");
-            gameState.updateScore(50);
-            eventBus.emit("updateUI", gameState.stats);
-            console.log(gameState.stats);
-        });
-    }
+    this.on("pointerdown", () => {
+      console.log("clicked floppydisk");
+      gameState.updateScore(50);
+      eventBus.emit("updateUI", gameState.stats);
+      console.log(gameState.stats);
+    });
+  }
 }
