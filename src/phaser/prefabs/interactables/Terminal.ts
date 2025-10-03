f734f7276926b92a17e854a0f77547aa650de9ac
Matt <designs.sleephawk@gmail.com>
2025-10-03 19:18:35 +0100

    changed scale of console to be more scary

--- src/phaser/prefabs/interactables/Terminal.ts
+++ src/phaser/prefabs/interactables/Terminal.ts
@@ -4,35 +4,36 @@ import type { Interactable } from "../../systems/interactableInterface";
 import type { Player } from "../characters/Player";
 
 export class Terminal extends BaseInteractable implements Interactable {
-    constructor(
-        scene: Phaser.Scene,
-        x: number,
-        y: number,
-        texture: string,
-        id: string | number
-    ) {
-        super(scene, x, y, texture, id);
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    texture: string,
+    id: string | number
+  ) {
+    super(scene, x, y, texture, id);
 
-        this.flipX = true;
-    }
-    interact() {
-        console.log("clicked terminal");
-        eventBus.emit("playerScored", 10);
-        eventBus.emit("terminalCompleted", this.id);
-    }
-    update(player: Player) {
-        const distance = Phaser.Math.Distance.Between(
-            this.x,
-            this.y,
-            player.x,
-            player.y
-        );
-        const inRange = distance < 300;
+    this.flipX = true;
+    this.scale = 2;
+  }
+  interact() {
+    console.log("clicked terminal");
+    eventBus.emit("playerScored", 10);
+    eventBus.emit("terminalCompleted", this.id);
+  }
+  update(player: Player) {
+    const distance = Phaser.Math.Distance.Between(
+      this.x,
+      this.y,
+      player.x,
+      player.y
+    );
+    const inRange = distance < 300;
 
-        if (inRange) {
-            this.setTexture("terminal-active");
-        } else {
-            this.setTexture("terminal");
-        }
+    if (inRange) {
+      this.setTexture("terminal-active");
+    } else {
+      this.setTexture("terminal");
     }
+  }
 }
